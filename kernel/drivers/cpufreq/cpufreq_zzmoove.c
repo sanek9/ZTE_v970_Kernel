88cf5258193c675cd337792c2d486d96d0caf130
dragonpt <cesar.maximo@gmail.com>
2014-04-07 13:38:03 +0100

    added CPU Governors and IO Shedulers

--- kernel/drivers/cpufreq/cpufreq_zzmoove.c
+++ kernel/drivers/cpufreq/cpufreq_zzmoove.c
@@ -1,5 +1,5 @@
 /*
- *  drivers/cpufreq/cpufreq_conservative.c
+ *  drivers/cpufreq/cpufreq_zzmoove.c
  *
  *  Copyright (C)  2001 Russell King
  *            (C)  2003 Venkatesh Pallipadi <venkatesh.pallipadi@intel.com>.
@@ -23,14 +23,23 @@
 #include <linux/tick.h>
 #include <linux/ktime.h>
 #include <linux/sched.h>
+#include <linux/earlysuspend.h>
+
+// smooth up/downscaling via lookup tables
+#define MN_SMOOTH 1
+
+// cpu load trigger
+#define DEF_SMOOTH_UP (75)
 
 /*
  * dbs is used in this file as a shortform for demandbased switching
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(80)
-#define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
+#define DEF_FREQUENCY_UP_THRESHOLD		(70)
+#define DEF_FREQUENCY_UP_THRESHOLD_HOTPLUG	(68)
+#define DEF_FREQUENCY_DOWN_THRESHOLD		(52)
+#define DEF_FREQUENCY_DOWN_THRESHOLD_HOTPLUG	(55)
 
 /*
  * The polling frequency of this governor depends on the capability of
@@ -46,9 +55,17 @@
 
 static unsigned int min_sampling_rate;
 
+// raise sampling rate to SR*multiplier on blank screen
+static unsigned int sampling_rate_awake;
+static unsigned int up_threshold_awake;
+static unsigned int down_threshold_awake;
+static unsigned int smooth_up_awake;
+
+#define SAMPLING_RATE_SLEEP_MULTIPLIER (2)
+
 #define LATENCY_MULTIPLIER			(1000)
 #define MIN_LATENCY_MULTIPLIER			(100)
-#define DEF_SAMPLING_DOWN_FACTOR		(1)
+#define DEF_SAMPLING_DOWN_FACTOR		(4)
 #define MAX_SAMPLING_DOWN_FACTOR		(10)
 #define TRANSITION_LATENCY_LIMIT		(10 * 1000 * 1000)
 
@@ -84,47 +101,127 @@ static struct dbs_tuners {
 	unsigned int sampling_rate;
 	unsigned int sampling_down_factor;
 	unsigned int up_threshold;
+	unsigned int up_threshold_hotplug;
 	unsigned int down_threshold;
+	unsigned int down_threshold_hotplug;
 	unsigned int ignore_nice;
 	unsigned int freq_step;
+	unsigned int smooth_up;
+
 } dbs_tuners_ins = {
 	.up_threshold = DEF_FREQUENCY_UP_THRESHOLD,
+	.up_threshold_hotplug = DEF_FREQUENCY_UP_THRESHOLD_HOTPLUG,
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
+	.down_threshold_hotplug = DEF_FREQUENCY_DOWN_THRESHOLD_HOTPLUG,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
 	.freq_step = 5,
+	.smooth_up = DEF_SMOOTH_UP,
 };
 
-static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
+/**
+ * Smooth scaling conservative governor
+ *            
+ * This modification makes the governor use two lookup tables holding
+ * current, next and previous frequency to directly get a correct
+ * target frequency instead of calculating target frequencies with
+ * up_threshold and step_up %. The two scaling lookup tables used 
+ * contain different scaling steps/frequencies to achieve faster upscaling 
+ * on higher CPU load.
+ * 
+ * CPU load triggering faster upscaling can be adjusted via SYSFS, 
+ * VALUE between 1 and 100 (% CPU load):
+ * echo VALUE > /sys/devices/system/cpu/cpufreq/conservative/smooth_up                
+ */
+
+#define MN_FREQ 0
+#define MN_UP 1
+#define MN_DOWN 2
+
+/* Table modified for use with Samsung I9300 by ZaneZam November 2012 */
+static int mn_freqs[13][3]={
+    {1400000,1400000,1300000},
+    {1300000,1400000,1200000},
+    {1200000,1300000,1100000},
+    {1100000,1200000,1000000},
+    {1000000,1100000, 900000},
+    { 900000,1000000, 800000},
+    { 800000, 900000, 700000},
+    { 700000, 800000, 600000},
+    { 600000, 700000, 400000},
+    { 500000, 600000, 300000},
+    { 400000, 500000, 200000},
+    { 300000, 400000, 200000},
+    { 200000, 300000, 200000}
+};
+
+/* Table modified for use with Samsung I9300 by ZaneZam November 2012 */
+static int mn_freqs_power[13][3]={
+    {1400000,1400000,1300000},
+    {1300000,1400000,1200000},
+    {1200000,1400000,1100000},
+    {1100000,1300000,1000000},
+    {1000000,1200000, 900000},
+    { 900000,1100000, 800000},
+    { 800000,1000000, 700000},
+    { 700000, 900000, 600000},
+    { 600000, 800000, 500000},
+    { 500000, 700000, 400000},
+    { 400000, 600000, 300000},
+    { 300000, 500000, 200000},
+    { 200000, 400000, 200000}
+};
+
+static int mn_get_next_freq(unsigned int curfreq, unsigned int updown, unsigned int load) {
+    int i=0;
+    if (load < dbs_tuners_ins.smooth_up)
+    {
+        for(i = 0; i < 13; i++)
+        {
+            if(curfreq == mn_freqs[i][MN_FREQ])
+                return mn_freqs[i][updown]; // updown 1|2
+        }
+    }
+    else
+    {
+        for(i = 0; i < 13; i++)
+        {
+            if(curfreq == mn_freqs_power[i][MN_FREQ])
+                return mn_freqs_power[i][updown]; // updown 1|2
+        }
+    }
+    return (curfreq); // not found
+    }
+
+static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
+							cputime64_t *wall)
 {
-	u64 idle_time;
-	u64 cur_wall_time;
-	u64 busy_time;
+	cputime64_t idle_time;
+	cputime64_t cur_wall_time;
+	cputime64_t busy_time;
 
 	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
+	busy_time = cputime64_add(kcpustat_cpu(cpu).cpustat[CPUTIME_USER],
+			kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM]);
 
-	busy_time  = kcpustat_cpu(cpu).cpustat[CPUTIME_USER];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SYSTEM];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL];
-	busy_time += kcpustat_cpu(cpu).cpustat[CPUTIME_NICE];
+	busy_time = cputime64_add(busy_time, kcpustat_cpu(cpu).cpustat[CPUTIME_IRQ]);
+	busy_time = cputime64_add(busy_time, kcpustat_cpu(cpu).cpustat[CPUTIME_SOFTIRQ]);
+	busy_time = cputime64_add(busy_time, kcpustat_cpu(cpu).cpustat[CPUTIME_STEAL]);
+	busy_time = cputime64_add(busy_time, kcpustat_cpu(cpu).cpustat[CPUTIME_NICE]);
 
-	idle_time = cur_wall_time - busy_time;
+	idle_time = cputime64_sub(cur_wall_time, busy_time);
 	if (wall)
-		*wall = jiffies_to_usecs(cur_wall_time);
+		*wall = (cputime64_t)jiffies_to_usecs(cur_wall_time);
 
-	return jiffies_to_usecs(idle_time);
+	return (cputime64_t)jiffies_to_usecs(idle_time);
 }
 
 static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
 {
-	u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
+	u64 idle_time = get_cpu_idle_time_us(cpu, wall);
 
 	if (idle_time == -1ULL)
 		return get_cpu_idle_time_jiffy(cpu, wall);
-	else
-		idle_time += get_cpu_iowait_time_us(cpu, wall);
 
 	return idle_time;
 }
@@ -170,7 +267,7 @@ static ssize_t show_sampling_rate_min(struct kobject *kobj,
 
 define_one_global_ro(sampling_rate_min);
 
-/* cpufreq_conservative Governor Tunables */
+/* cpufreq_zzmoove Governor Tunables */
 #define show_one(file_name, object)					\
 static ssize_t show_##file_name						\
 (struct kobject *kobj, struct attribute *attr, char *buf)		\
@@ -180,9 +277,12 @@ static ssize_t show_##file_name						\
 show_one(sampling_rate, sampling_rate);
 show_one(sampling_down_factor, sampling_down_factor);
 show_one(up_threshold, up_threshold);
+show_one(up_threshold_hotplug, up_threshold_hotplug);
 show_one(down_threshold, down_threshold);
+show_one(down_threshold_hotplug, down_threshold_hotplug);
 show_one(ignore_nice_load, ignore_nice);
 show_one(freq_step, freq_step);
+show_one(smooth_up, smooth_up);
 
 static ssize_t store_sampling_down_factor(struct kobject *a,
 					  struct attribute *b,
@@ -228,6 +328,21 @@ static ssize_t store_up_threshold(struct kobject *a, struct attribute *b,
 	return count;
 }
 
+static ssize_t store_up_threshold_hotplug(struct kobject *a, struct attribute *b,
+				  const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > 100 ||
+			input <= dbs_tuners_ins.down_threshold)
+		return -EINVAL;
+
+	dbs_tuners_ins.up_threshold_hotplug = input;
+	return count;
+}
+
 static ssize_t store_down_threshold(struct kobject *a, struct attribute *b,
 				    const char *buf, size_t count)
 {
@@ -241,6 +356,22 @@ static ssize_t store_down_threshold(struct kobject *a, struct attribute *b,
 		return -EINVAL;
 
 	dbs_tuners_ins.down_threshold = input;
+	return count;
+}
+
+static ssize_t store_down_threshold_hotplug(struct kobject *a, struct attribute *b,
+				    const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	/* cannot be lower than 11 otherwise freq will not fall */
+	if (ret != 1 || input < 11 || input > 100 ||
+			input >= dbs_tuners_ins.up_threshold)
+		return -EINVAL;
+
+	dbs_tuners_ins.down_threshold_hotplug = input;
 	return count;
 }
 
@@ -295,27 +426,48 @@ static ssize_t store_freq_step(struct kobject *a, struct attribute *b,
 	return count;
 }
 
+static ssize_t store_smooth_up(struct kobject *a,
+					  struct attribute *b,
+					  const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (ret != 1 || input > 100 || input < 1)
+		return -EINVAL;
+
+	dbs_tuners_ins.smooth_up = input;
+	return count;
+}
+
 define_one_global_rw(sampling_rate);
 define_one_global_rw(sampling_down_factor);
 define_one_global_rw(up_threshold);
+define_one_global_rw(up_threshold_hotplug);
 define_one_global_rw(down_threshold);
+define_one_global_rw(down_threshold_hotplug);
 define_one_global_rw(ignore_nice_load);
 define_one_global_rw(freq_step);
+define_one_global_rw(smooth_up);
 
 static struct attribute *dbs_attributes[] = {
 	&sampling_rate_min.attr,
 	&sampling_rate.attr,
 	&sampling_down_factor.attr,
-	&up_threshold.attr,
+	&up_threshold_hotplug.attr,
 	&down_threshold.attr,
+	&down_threshold_hotplug.attr,
 	&ignore_nice_load.attr,
 	&freq_step.attr,
+	&smooth_up.attr,
+	&up_threshold.attr,
 	NULL
 };
 
 static struct attribute_group dbs_attr_group = {
 	.attrs = dbs_attributes,
-	.name = "conservative",
+	.name = "zzmoove",
 };
 
 /************************** sysfs end ************************/
@@ -324,7 +476,6 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 {
 	unsigned int load = 0;
 	unsigned int max_load = 0;
-	unsigned int freq_target;
 
 	struct cpufreq_policy *policy;
 	unsigned int j;
@@ -352,20 +503,20 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 		cur_idle_time = get_cpu_idle_time(j, &cur_wall_time);
 
-		wall_time = (unsigned int)
-			(cur_wall_time - j_dbs_info->prev_cpu_wall);
+		wall_time = (unsigned int) cputime64_sub(cur_wall_time,
+				j_dbs_info->prev_cpu_wall);
 		j_dbs_info->prev_cpu_wall = cur_wall_time;
 
-		idle_time = (unsigned int)
-			(cur_idle_time - j_dbs_info->prev_cpu_idle);
+		idle_time = (unsigned int) cputime64_sub(cur_idle_time,
+				j_dbs_info->prev_cpu_idle);
 		j_dbs_info->prev_cpu_idle = cur_idle_time;
 
 		if (dbs_tuners_ins.ignore_nice) {
-			u64 cur_nice;
+			cputime64_t cur_nice;
 			unsigned long cur_nice_jiffies;
 
-			cur_nice = kcpustat_cpu(j).cpustat[CPUTIME_NICE] -
-					 j_dbs_info->prev_cpu_nice;
+			cur_nice = cputime64_sub(kcpustat_cpu(j).cpustat[CPUTIME_NICE],
+					 j_dbs_info->prev_cpu_nice);
 			/*
 			 * Assumption: nice time between sampling periods will
 			 * be less than 2^32 jiffies for 32 bit sys
@@ -393,6 +544,18 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	if (dbs_tuners_ins.freq_step == 0)
 		return;
 
+	/*
+	 * Check for frequency increase is greater than hotplug value and wake up cpus accordingly
+	 * Following will bring up 3 cores in a row (cpu0 is always on!) - not optimal but it works for now
+	 * Modification by ZaneZam November 2012
+	 */
+	
+	if (num_online_cpus() < 2 && max_load > dbs_tuners_ins.up_threshold_hotplug) {
+			mutex_unlock(&this_dbs_info->timer_mutex); /* this seems to be a very good idea, without it lockups are possible! */
+			cpu_up(1);
+			mutex_lock(&this_dbs_info->timer_mutex); /* this seems to be a very good idea, without it lockups are possible! */
+	} 
+	
 	/* Check for frequency increase */
 	if (max_load > dbs_tuners_ins.up_threshold) {
 		this_dbs_info->down_skip = 0;
@@ -401,15 +564,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		if (this_dbs_info->requested_freq == policy->max)
 			return;
 
-		freq_target = (dbs_tuners_ins.freq_step * policy->max) / 100;
-
-		/* max freq cannot be less than 100. But who knows.... */
-		if (unlikely(freq_target == 0))
-			freq_target = 5;
-
-		this_dbs_info->requested_freq += freq_target;
-		if (this_dbs_info->requested_freq > policy->max)
-			this_dbs_info->requested_freq = policy->max;
+        this_dbs_info->requested_freq = mn_get_next_freq(policy->cur, MN_UP, max_load);
 
 		__cpufreq_driver_target(policy, this_dbs_info->requested_freq,
 			CPUFREQ_RELATION_H);
@@ -417,22 +572,31 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	}
 
 	/*
+	 * Check for frequency decrease is lower than hotplug value and put cpus to sleep accordingly
+	 * Following will disable 3 cores in a row (cpu0 is always on!) - not optimal but it works for now
+	 * Modification by ZaneZam November 2012
+	 */
+	
+	if (num_online_cpus() > 1 && max_load < dbs_tuners_ins.down_threshold_hotplug) {
+			mutex_unlock(&this_dbs_info->timer_mutex); /* this seems to be a very good idea, without it lockups are possible */
+			cpu_down(1);
+			mutex_lock(&this_dbs_info->timer_mutex); /* this seems to be a very good idea, without it lockups are possible */
+	} 
+
+	/*
 	 * The optimal frequency is the frequency that is the lowest that
 	 * can support the current CPU usage without triggering the up
 	 * policy. To be safe, we focus 10 points under the threshold.
 	 */
 	if (max_load < (dbs_tuners_ins.down_threshold - 10)) {
-		freq_target = (dbs_tuners_ins.freq_step * policy->max) / 100;
-
-		this_dbs_info->requested_freq -= freq_target;
-		if (this_dbs_info->requested_freq < policy->min)
-			this_dbs_info->requested_freq = policy->min;
 
 		/*
 		 * if we cannot reduce the frequency anymore, break out early
 		 */
 		if (policy->cur == policy->min)
 			return;
+
+        this_dbs_info->requested_freq = mn_get_next_freq(policy->cur, MN_DOWN, max_load);
 
 		__cpufreq_driver_target(policy, this_dbs_info->requested_freq,
 				CPUFREQ_RELATION_H);
@@ -476,6 +640,36 @@ static inline void dbs_timer_exit(struct cpu_dbs_info_s *dbs_info)
 	cancel_delayed_work_sync(&dbs_info->work);
 }
 
+static void powersave_early_suspend(struct early_suspend *handler)
+{
+  mutex_lock(&dbs_mutex);
+  sampling_rate_awake = dbs_tuners_ins.sampling_rate;
+  up_threshold_awake = dbs_tuners_ins.up_threshold;
+  down_threshold_awake = dbs_tuners_ins.down_threshold;
+  smooth_up_awake = dbs_tuners_ins.smooth_up;
+  dbs_tuners_ins.sampling_rate *= SAMPLING_RATE_SLEEP_MULTIPLIER;
+  dbs_tuners_ins.up_threshold = 90;
+  dbs_tuners_ins.down_threshold = 44;
+  dbs_tuners_ins.smooth_up = 100;
+  mutex_unlock(&dbs_mutex);
+}
+
+static void powersave_late_resume(struct early_suspend *handler)
+{
+  mutex_lock(&dbs_mutex);
+  dbs_tuners_ins.sampling_rate = sampling_rate_awake;
+  dbs_tuners_ins.up_threshold = up_threshold_awake;
+  dbs_tuners_ins.down_threshold = down_threshold_awake;
+  dbs_tuners_ins.smooth_up = smooth_up_awake;
+  mutex_unlock(&dbs_mutex);
+}
+
+static struct early_suspend _powersave_early_suspend = {
+  .suspend = powersave_early_suspend,
+  .resume = powersave_late_resume,
+  .level = EARLY_SUSPEND_LEVEL_BLANK_SCREEN,
+};
+
 static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 				   unsigned int event)
 {
@@ -500,9 +694,10 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 
 			j_dbs_info->prev_cpu_idle = get_cpu_idle_time(j,
 						&j_dbs_info->prev_cpu_wall);
-			if (dbs_tuners_ins.ignore_nice)
+			if (dbs_tuners_ins.ignore_nice) {
 				j_dbs_info->prev_cpu_nice =
 						kcpustat_cpu(j).cpustat[CPUTIME_NICE];
+			}
 		}
 		this_dbs_info->down_skip = 0;
 		this_dbs_info->requested_freq = policy->cur;
@@ -531,9 +726,18 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			 * conservative does not implement micro like ondemand
 			 * governor, thus we are bound to jiffes/HZ
 			 */
-			min_sampling_rate = 10000;
-			dbs_tuners_ins.sampling_rate = min_sampling_rate;
-
+			min_sampling_rate =
+				MIN_SAMPLING_RATE_RATIO * jiffies_to_usecs(3);
+			/* Bring kernel and HW constraints together */
+			min_sampling_rate = max(min_sampling_rate,
+					MIN_LATENCY_MULTIPLIER * latency);
+			dbs_tuners_ins.sampling_rate =
+				max(min_sampling_rate,
+				    latency * LATENCY_MULTIPLIER);
+			sampling_rate_awake = dbs_tuners_ins.sampling_rate;
+			up_threshold_awake = dbs_tuners_ins.up_threshold;
+			down_threshold_awake = dbs_tuners_ins.down_threshold;
+			smooth_up_awake = dbs_tuners_ins.smooth_up;
 			cpufreq_register_notifier(
 					&dbs_cpufreq_notifier_block,
 					CPUFREQ_TRANSITION_NOTIFIER);
@@ -541,7 +745,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 		mutex_unlock(&dbs_mutex);
 
 		dbs_timer_init(this_dbs_info);
-
+        register_early_suspend(&_powersave_early_suspend);
 		break;
 
 	case CPUFREQ_GOV_STOP:
@@ -565,6 +769,7 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			sysfs_remove_group(cpufreq_global_kobject,
 					   &dbs_attr_group);
 
+        unregister_early_suspend(&_powersave_early_suspend);
 		break;
 
 	case CPUFREQ_GOV_LIMITS:
@@ -584,11 +789,11 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 	return 0;
 }
 
-#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE
+#ifndef CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE
 static
 #endif
-struct cpufreq_governor cpufreq_gov_conservative = {
-	.name			= "conservative",
+struct cpufreq_governor cpufreq_gov_zzmoove = {
+	.name			= "zzmoove",
 	.governor		= cpufreq_governor_dbs,
 	.max_transition_latency	= TRANSITION_LATENCY_LIMIT,
 	.owner			= THIS_MODULE,
@@ -596,22 +801,30 @@ struct cpufreq_governor cpufreq_gov_conservative = {
 
 static int __init cpufreq_gov_dbs_init(void)
 {
-	return cpufreq_register_governor(&cpufreq_gov_conservative);
+	return cpufreq_register_governor(&cpufreq_gov_zzmoove);
 }
 
 static void __exit cpufreq_gov_dbs_exit(void)
 {
-	cpufreq_unregister_governor(&cpufreq_gov_conservative);
+	cpufreq_unregister_governor(&cpufreq_gov_zzmoove);
 }
 
+// zzmoove is based on the modified conservative (original author
+// Alexander Clouter <alex@digriz.org.uk>) smoove governor from Michael Weingaertner <mialwe@googlemail.com>
+// (source: https://github.com/mialwe/mngb/)
+// Modified by Zane Zaminsky November 2012 to be hotplug-able and optimzed for use with Samsung I9300.
+// CPU Hotplug modifications partially taken from ktoonservative governor from
+// ktoonsez KT747-JB kernel (https://github.com/ktoonsez/KT747-JB)
 
-MODULE_AUTHOR("Alexander Clouter <alex@digriz.org.uk>");
-MODULE_DESCRIPTION("'cpufreq_conservative' - A dynamic cpufreq governor for "
-		"Low Latency Frequency Transition capable processors "
-		"optimised for use in a battery environment");
+MODULE_AUTHOR("Zane Zaminsky <cyxman@yahoo.com>");
+MODULE_DESCRIPTION("'cpufreq_zzmoove' - A dynamic cpufreq governor based "
+		"on smoove governor from Michael Weingaertner which was originally based on "
+		"cpufreq_conservative from Alexander Clouter. Optimized for use with Samsung I9300 "
+		"using frequency lookup tables and CPU hotplug - ported/modified for I9300 "
+		"by ZaneZam November 2012");
 MODULE_LICENSE("GPL");
 
-#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE
+#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE
 fs_initcall(cpufreq_gov_dbs_init);
 #else
 module_init(cpufreq_gov_dbs_init);
