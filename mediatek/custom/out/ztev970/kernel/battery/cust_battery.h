fb9ac3f4ea58caa52e578f874dcb61665c4becfa
dragonpt <cesar.maximo@gmail.com>
2014-06-16 23:28:45 +0100

    mm: fix wrong argument of migrate_huge_pages() in soft_offline_huge_page()

--- mediatek/custom/out/ztev970/kernel/battery/cust_battery.h
+++ mediatek/custom/out/ztev970/kernel/battery/cust_battery.h
@@ -19,7 +19,7 @@ typedef enum
 	Cust_CC_400MA  = 0xD,
 	Cust_CC_200MA  = 0xE,
 	Cust_CC_70MA   = 0xF,
-	Cust_CC_0MA	   = 0xDD
+	Cust_CC_0MA    = 0xDD
 }cust_charging_current_enum;
 
 typedef struct{	
@@ -28,20 +28,21 @@ typedef struct{
 }VBAT_TO_PERCENT;
 
 /* Battery Temperature Protection */
-#define MAX_CHARGE_TEMPERATURE  65
-#define MIN_CHARGE_TEMPERATURE  -20
+#define MAX_CHARGE_TEMPERATURE  50
+#define MIN_CHARGE_TEMPERATURE  -13
 #define ERR_CHARGE_TEMPERATURE  0xFF
+#define MAX_BATT_TEMPERATURE	55
 
 /* Recharging Battery Voltage */
 #define RECHARGING_VOLTAGE      4110
 
 /* Charging Current Setting */
-#define CONFIG_USB_IF 						0   
-#define USB_CHARGER_CURRENT_SUSPEND			Cust_CC_0MA		// def CONFIG_USB_IF
+#define CONFIG_USB_IF 				0   
+#define USB_CHARGER_CURRENT_SUSPEND		Cust_CC_0MA	// def CONFIG_USB_IF
 #define USB_CHARGER_CURRENT_UNCONFIGURED	Cust_CC_70MA	// def CONFIG_USB_IF
 #define USB_CHARGER_CURRENT_CONFIGURED		Cust_CC_450MA	// def CONFIG_USB_IF
-#define USB_CHARGER_CURRENT					Cust_CC_450MA
-#define AC_CHARGER_CURRENT					Cust_CC_650MA //Cust_CC_700MA   //Cust_CC_650MA	
+#define USB_CHARGER_CURRENT			Cust_CC_450MA
+#define AC_CHARGER_CURRENT			Cust_CC_700MA	
 
 /* Battery Meter Solution */
 #define CONFIG_ADC_SOLUTION 	1
@@ -67,29 +68,30 @@ VBAT_TO_PERCENT Batt_VoltToPercent_Table[] = {
 #define BATTERY_AVERAGE_SIZE   12
 
 /* Common setting */
-#define R_CURRENT_SENSE 2				// 0.2 Ohm
-#define R_BAT_SENSE 4					// times of voltage
-#define R_I_SENSE 4						// times of voltage
-#define R_CHARGER_1 330
-#define R_CHARGER_2 39
+#define R_CURRENT_SENSE		2	// 0.2 Ohm
+#define R_BAT_SENSE		4	// times of voltage
+#define R_I_SENSE		4	// times of voltage
+#define R_CHARGER_1		330
+#define R_CHARGER_2		39
 #define R_CHARGER_SENSE ((R_CHARGER_1+R_CHARGER_2)/R_CHARGER_2)	// times of voltage
-//#define V_CHARGER_MAX 6000				// 6 V
-#define V_CHARGER_MAX 6500				// 6.5 V
-#define V_CHARGER_MIN 4400				// 4.4 V
-#define V_CHARGER_ENABLE 0				// 1:ON , 0:OFF
+//#define V_CHARGER_MAX		6000	// 6 V
+#define V_CHARGER_MAX		6500	// 6.5 V
+#define V_CHARGER_MIN		4400	// 4.4 V
+#define V_CHARGER_ENABLE	0	// 1:ON , 0:OFF
 
 /* Teperature related setting */
 #define RBAT_PULL_UP_R             24000
 //#define RBAT_PULL_UP_VOLT        2500
 #define RBAT_PULL_UP_VOLT          1200
 //#define TBAT_OVER_CRITICAL_LOW   68237
-
 //#define TBAT_OVER_CRITICAL_LOW   483954
 #define TBAT_OVER_CRITICAL_LOW     70603
 #define BAT_TEMP_PROTECT_ENABLE    0
-//#define BAT_NTC_10 		   1  
-#define BAT_NTC_47 		   1
-//#define BAT_NTC_TSM_1 
+
+//#define BAT_NTC_TSM_1	  	   
+//#define BAT_NTC_10_SEN_1	    
+//#define BAT_NTC_10		   1 // 1:ON , 0:OFF
+#define BAT_NTC_47		   1 // 1:ON , 0:OFF
 
 /* Battery Notify */
 #define BATTERY_NOTIFY_CASE_0001
